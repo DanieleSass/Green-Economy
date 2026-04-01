c0448c1ebdfa2948e94f61cf5cecbe2cec61659e
DanieleSass <12999345@itisrossi.vi.it>
2026-04-01 13:07:04 +0200

    mettere componentin grafico
    .

---
+++ Green Economy/Green Economy/CInfo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Green_Economy
+{
+    internal class CInfo
+    {
+        DateTime data;
+        float temperatura;
+
+        public CInfo()
+        {
+
+        }
+    }
+}
--- Green Economy/Green Economy/Form1.cs
+++ Green Economy/Green Economy/Form1.cs
@@ -1,11 +1,52 @@
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Newtonsoft.Json;
+
 namespace Green_Economy
 {
     public partial class Form1 : Form
     {
+        List<CInfo> lista;
+        string path;
         public Form1()
         {
             InitializeComponent();
-            //ciao
+            lista = new();
+            path = Path.Combine(Directory.GetCurrentDirectory(), "../../File/dati.json");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ScriviFile()
+        {
+
+            //fare controllo se esiste cartella e file, altrimenti crearli
+            try
+            {
+                string txt = JsonConvert.SerializeObject(lista, Formatting.Indented);
+                File.WriteAllText(path, txt);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Errore: " + ex.Message);
+            }
+        }
+        private void LeggiFile()
+        {
+            //fare controllo se esiste cartella e file, altrimenti crearli
+            try
+            {
+                string txt = File.ReadAllText(path);
+                List<CInfo> lista = JsonConvert.DeserializeObject<List<CInfo>>(txt);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Errore: " + ex.Message);
+            }
+
         }
     }
 }
